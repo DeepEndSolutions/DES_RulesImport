790fdc962eeb5aafe207c228700de5ec4e7aff15
mwdes <mike.williams@deependsolutions.com>
2016-10-27 16:36:28 -0600

    Changed to VB

---
+++ frmMain.Designer.vb
@@ -0,0 +1,152 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.btnImport = New System.Windows.Forms.Button()
+        Me.pbImportProgress = New System.Windows.Forms.ProgressBar()
+        Me.txtInputFile = New System.Windows.Forms.TextBox()
+        Me.lblInput = New System.Windows.Forms.Label()
+        Me.btnInputFile = New System.Windows.Forms.Button()
+        Me.lblOutputFile = New System.Windows.Forms.Label()
+        Me.txtOutputFile = New System.Windows.Forms.TextBox()
+        Me.btnOutputFile = New System.Windows.Forms.Button()
+        Me.lblProgressBar = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnImport
+        '
+        Me.btnImport.Location = New System.Drawing.Point(94, 151)
+        Me.btnImport.Name = "btnImport"
+        Me.btnImport.Size = New System.Drawing.Size(75, 38)
+        Me.btnImport.TabIndex = 0
+        Me.btnImport.Text = "Import"
+        Me.btnImport.UseVisualStyleBackColor = True
+        '
+        'pbImportProgress
+        '
+        Me.pbImportProgress.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.pbImportProgress.Location = New System.Drawing.Point(13, 227)
+        Me.pbImportProgress.Name = "pbImportProgress"
+        Me.pbImportProgress.Size = New System.Drawing.Size(305, 23)
+        Me.pbImportProgress.TabIndex = 1
+        '
+        'txtInputFile
+        '
+        Me.txtInputFile.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtInputFile.Location = New System.Drawing.Point(13, 48)
+        Me.txtInputFile.Name = "txtInputFile"
+        Me.txtInputFile.Size = New System.Drawing.Size(280, 20)
+        Me.txtInputFile.TabIndex = 2
+        '
+        'lblInput
+        '
+        Me.lblInput.AutoSize = True
+        Me.lblInput.Location = New System.Drawing.Point(13, 29)
+        Me.lblInput.Name = "lblInput"
+        Me.lblInput.Size = New System.Drawing.Size(47, 13)
+        Me.lblInput.TabIndex = 3
+        Me.lblInput.Text = "Input file"
+        '
+        'btnInputFile
+        '
+        Me.btnInputFile.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnInputFile.Location = New System.Drawing.Point(299, 46)
+        Me.btnInputFile.Name = "btnInputFile"
+        Me.btnInputFile.Size = New System.Drawing.Size(28, 23)
+        Me.btnInputFile.TabIndex = 4
+        Me.btnInputFile.Text = "..."
+        Me.btnInputFile.UseVisualStyleBackColor = True
+        '
+        'lblOutputFile
+        '
+        Me.lblOutputFile.AutoSize = True
+        Me.lblOutputFile.Location = New System.Drawing.Point(13, 83)
+        Me.lblOutputFile.Name = "lblOutputFile"
+        Me.lblOutputFile.Size = New System.Drawing.Size(55, 13)
+        Me.lblOutputFile.TabIndex = 6
+        Me.lblOutputFile.Text = "Output file"
+        '
+        'txtOutputFile
+        '
+        Me.txtOutputFile.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtOutputFile.Location = New System.Drawing.Point(13, 102)
+        Me.txtOutputFile.Name = "txtOutputFile"
+        Me.txtOutputFile.Size = New System.Drawing.Size(280, 20)
+        Me.txtOutputFile.TabIndex = 5
+        '
+        'btnOutputFile
+        '
+        Me.btnOutputFile.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnOutputFile.Location = New System.Drawing.Point(299, 100)
+        Me.btnOutputFile.Name = "btnOutputFile"
+        Me.btnOutputFile.Size = New System.Drawing.Size(28, 23)
+        Me.btnOutputFile.TabIndex = 7
+        Me.btnOutputFile.Text = "..."
+        Me.btnOutputFile.UseVisualStyleBackColor = True
+        '
+        'lblProgressBar
+        '
+        Me.lblProgressBar.AutoSize = True
+        Me.lblProgressBar.Location = New System.Drawing.Point(13, 211)
+        Me.lblProgressBar.Name = "lblProgressBar"
+        Me.lblProgressBar.Size = New System.Drawing.Size(116, 13)
+        Me.lblProgressBar.TabIndex = 8
+        Me.lblProgressBar.Text = "Click Import to Begin...."
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(330, 262)
+        Me.Controls.Add(Me.lblProgressBar)
+        Me.Controls.Add(Me.btnOutputFile)
+        Me.Controls.Add(Me.lblOutputFile)
+        Me.Controls.Add(Me.txtOutputFile)
+        Me.Controls.Add(Me.btnInputFile)
+        Me.Controls.Add(Me.lblInput)
+        Me.Controls.Add(Me.txtInputFile)
+        Me.Controls.Add(Me.pbImportProgress)
+        Me.Controls.Add(Me.btnImport)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Rules Import"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnImport As Button
+    Friend WithEvents pbImportProgress As ProgressBar
+    Friend WithEvents txtInputFile As TextBox
+    Friend WithEvents lblInput As Label
+    Friend WithEvents btnInputFile As Button
+    Friend WithEvents lblOutputFile As Label
+    Friend WithEvents txtOutputFile As TextBox
+    Friend WithEvents btnOutputFile As Button
+    Friend WithEvents lblProgressBar As Label
+End Class
---
+++ frmMain.vb
@@ -0,0 +1,177 @@
+﻿Imports Excel = Microsoft.Office.Interop.Excel
+Imports System.IO
+
+Public Class frmMain
+    Private Sub btnImport_Click(sender As Object, e As EventArgs) Handles btnImport.Click
+
+        Dim oExcel As Excel.Application
+        'Start an excel object
+        oExcel = CreateObject("Excel.Application")
+
+        Dim oWorkbook As Excel.Workbook
+        Dim oWorksheet As Excel.Worksheet = Nothing
+
+        oWorkbook = oExcel.Workbooks.Open(txtInputFile.Text)
+
+        Cursor = Cursors.WaitCursor
+
+        For Each oWorksheet In oWorkbook.Sheets
+            InsertRules(oWorksheet)
+        Next
+
+        lblProgressBar.Text = "Rules import Complete!"
+
+        'Close and exit Excel
+        oWorkbook.Close(True)
+        oExcel.Quit()
+
+        'release all COM objects
+        System.Runtime.InteropServices.Marshal.ReleaseComObject(oWorksheet)
+        oWorksheet = Nothing
+        System.Runtime.InteropServices.Marshal.ReleaseComObject(oWorkbook)
+        oWorkbook = Nothing
+        System.Runtime.InteropServices.Marshal.ReleaseComObject(oExcel)
+        oExcel = Nothing
+
+        GC.Collect()
+
+        Cursor = Cursors.Default
+    End Sub
+
+    Public Function InsertRules(ByVal oWorksheet As Excel.Worksheet) As Integer
+        Try
+            Dim oCell As Excel.Range
+            Dim intFor As Integer = 2
+            Dim intRows As Integer = 0
+            Dim NumberOfRowsUpdated As Integer = 0
+            Dim strRuleInsert As String = ""
+            Dim strRule As String = ""
+            Dim strRuleDescription As String = ""
+            Dim strRuleSubSection As String = ""
+            Dim strRuleSubSectionDescription As String = ""
+
+
+            'make sure there is at least on row of data but skip first row since it should be header
+            If oWorksheet.Range("A" & oWorksheet.Rows.Count).End(Excel.XlDirection.xlUp).Row >= 2 Then
+                intRows = oWorksheet.Range("A" & oWorksheet.Rows.Count).End(Excel.XlDirection.xlUp).Row
+
+                pbImportProgress.Minimum = 0
+                pbImportProgress.Maximum = intRows
+                pbImportProgress.Value = 0
+                pbImportProgress.Step = 1
+                lblProgressBar.Text = "Begin Rules import......."
+
+                'Write each section name as a comment
+                WriteToRuleInsert("-- " & oWorksheet.Name)
+
+                'loop through all rows but use "A" as guide since that field cannot be empty
+                For intFor = 2 To intRows
+
+                    'clear these each time
+                    strRuleSubSection = ""
+
+                    'get each cell and put into field for the Section
+                    oCell = oWorksheet.Range("A" & intFor)
+                    If oCell.Value IsNot Nothing Then
+                        strRule = oCell.Value.ToString.Substring(0, oCell.Value.ToString.IndexOf(" "))
+                        strRuleDescription = oCell.Value.ToString.Substring(oCell.Value.ToString.IndexOf(" ") + 1)
+                        strRuleInsert = "INSERT INTO RuleSection VALUES ('" & strRule & "','" & strRuleDescription & "')"
+                        'Writeline
+                        WriteToRuleInsert(strRuleInsert)
+                    End If
+
+                    'get each cell and put into field for the SubSection and description
+                    oCell = oWorksheet.Range("B" & intFor)
+                    If oCell.Value IsNot Nothing Then
+                        strRuleSubSection = oCell.Value.ToString
+                        oCell = oWorksheet.Range("C" & intFor)
+                        If oCell.Value IsNot Nothing Then
+                            strRuleSubSectionDescription = oCell.Value
+                        End If
+                        'Replace " with blank char and replace newlines (char(10) and char(13)) with a space
+                        strRuleSubSectionDescription = strRuleSubSectionDescription.Replace(Chr(34), "").Replace(Chr(10), " ").Replace(Chr(13), " ")
+                        strRuleInsert = "INSERT INTO RuleSubSection VALUES ('" & strRule & "','" & strRuleSubSection & "','" & strRuleSubSectionDescription & "')"
+                        'Writeline
+                        WriteToRuleInsert(strRuleInsert)
+                    End If
+                    pbImportProgress.PerformStep()
+
+                Next
+            End If
+
+            'Add new line with SQL "GO" command
+            WriteToRuleInsert("GO" & vbCrLf)
+            lblProgressBar.Text = "Finished " & oWorksheet.Name
+            pbImportProgress.Value = pbImportProgress.Maximum
+
+            Return NumberOfRowsUpdated
+
+
+        Catch ex As Exception
+
+            Return 0
+        End Try
+    End Function
+
+    Private Sub WriteToRuleInsert(ByRef strInfo As String)
+        Try
+            Dim strOutputPath As String = txtOutputFile.Text
+
+            Dim objSW As StreamWriter = New StreamWriter(txtOutputFile.Text, True)
+
+            objSW.WriteLine(strInfo)
+
+            objSW.Close()
+
+        Catch ex As Exception
+            MessageBox.Show("An error has occured in the Procedure: WriteToRuleInsert " & vbCrLf &
+                ex.Message, "Unexpected Program Error", MessageBoxButtons.OK,
+                MessageBoxIcon.Error)
+        End Try
+
+    End Sub
+
+    Private Sub btnInputFile_Click(sender As Object, e As EventArgs) Handles btnInputFile.Click
+
+        'open dialog file
+        Dim oFileDialog As New OpenFileDialog
+        Dim oFileInfo As FileInfo
+
+        If txtInputFile.Text <> "" Then
+            oFileInfo = New FileInfo(txtInputFile.Text)
+            oFileDialog.InitialDirectory = oFileInfo.Directory.ToString
+        End If
+
+        oFileDialog.Filter = "MS EXCEL files (*.xls;*.xlsx)|*.xls;*.xlsx"
+
+        oFileDialog.ShowDialog()
+
+        'Open import file unless cancelled
+        If oFileDialog.FileName = "" Then
+            Exit Sub
+        Else
+            txtInputFile.Text = oFileDialog.FileName
+        End If
+
+    End Sub
+
+    Private Sub btnOutputFile_Click(sender As Object, e As EventArgs) Handles btnOutputFile.Click
+        'open dialog file
+        Dim oFileDialog As New SaveFileDialog
+
+
+        oFileDialog.Filter = "SQL Server (*.sql)|*.sql"
+
+        oFileDialog.ShowDialog()
+
+        'Open import file unless cancelled
+        If oFileDialog.FileName = "" Then
+            Exit Sub
+        Else
+            If File.Exists(oFileDialog.FileName) Then
+                File.Delete(oFileDialog.FileName)
+            End If
+            txtOutputFile.Text = oFileDialog.FileName
+        End If
+    End Sub
+End Class
